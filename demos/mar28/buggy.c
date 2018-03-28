28cab8f0b73a03be885151d4acc917eed0b73cee
Yuzhe Tang <tristartom@gmail.com>
2018-03-28 18:21:57 -0400

    regular updates

--- demos/mar28/buggy.c
+++ demos/mar28/buggy.c
@@ -1,10 +1,9 @@
 #include<stdio.h> //printf
 
 int array_stack[] = {0,1,2};
-
 int main(){
-  int sum; // local variable
-  for(int i=0; i<=3; i++){
+  int sum = 0; // local variable
+  for(int i=0; i<3; i++){
     sum += array_stack[i];
   }
   printf("sum = %d\n", sum);
